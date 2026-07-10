8e1f20b60dd0f3d41155f1c4f6d139a637b4fe04
scotex1 <satyampal8303@gmail.com>
2026-07-10 22:06:50 +0300

    Add files via upload

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,1 +1,12 @@
+import { useAuthStore } from '@/store/authStore'
+import { isPlanExpired, daysRemaining } from '@/lib/utils'
 
+export function useAuth() {
+  const state = useAuthStore()
+  return {
+    ...state,
+    firebaseUser: state.uid ? { uid: state.uid } : null,
+    isExpired: isPlanExpired(state.planExpiry),
+    daysLeft: daysRemaining(state.planExpiry),
+  }
+}
